fc269e8974c4073e4054c02537fc5ac2635bffc7
LeeWonGun <wongun1850@gmail.com>
2026-07-02 17:18:44 +0900

    fix: 2026-07-02 v4 스키마 오류 해결

--- troublelog_backend/src/main/resources/db/migration/V4__add_id_to_question_tech_stacks.sql
+++ troublelog_backend/src/main/resources/db/migration/V4__add_id_to_question_tech_stacks.sql
@@ -1,4 +1,10 @@
 ALTER TABLE question_tech_stacks
+    DROP FOREIGN KEY fk_question_tech_stacks_question;
+
+ALTER TABLE question_tech_stacks
+    DROP FOREIGN KEY fk_question_tech_stacks_tech_stack;
+
+ALTER TABLE question_tech_stacks
     DROP PRIMARY KEY;
 
 ALTER TABLE question_tech_stacks
@@ -7,3 +13,13 @@ ALTER TABLE question_tech_stacks
 ALTER TABLE question_tech_stacks
     ADD CONSTRAINT uk_question_tech_stacks_question_tech
         UNIQUE (question_id, tech_stack_id);
+
+ALTER TABLE question_tech_stacks
+    ADD CONSTRAINT fk_question_tech_stacks_question
+        FOREIGN KEY (question_id)
+            REFERENCES questions (id);
+
+ALTER TABLE question_tech_stacks
+    ADD CONSTRAINT fk_question_tech_stacks_tech_stack
+        FOREIGN KEY (tech_stack_id)
+            REFERENCES tech_stacks (id);
